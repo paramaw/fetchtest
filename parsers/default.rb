7d93267c771851718765b0b228f0a5958ca70ab6
Parama Danoesubroto <paramaw@gmail.com>
2021-06-03 19:21:08 +0300

    fix nil error

--- parsers/default.rb
+++ parsers/default.rb
@@ -3,9 +3,10 @@ nokogiri = Nokogiri.HTML(content)
 
 text = nokogiri.text
 
+
 doc1 = {
     text: text,
-    proxy_type: page['vars']['proxy_type']
+    proxy_type: page['vars'] ? page['vars']['proxy_type'] : nil
 }
 
 outputs << doc1
